82b2ce713510b7077919c9ccba366fb18d7f4cf0
Siddhantagarwal0401 <65842868+Siddhantagarwal0401@users.noreply.github.com>
2025-05-26 23:06:34 +0530

    improved ui

--- app/quiz.tsx
+++ app/quiz.tsx
@@ -427,4 +427,16 @@ const styles = StyleSheet.create({
   placeholderText: { 
     fontSize: 16,
   },
+  cardShadow: { 
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4, // for Android
+  },
 });
+
+// export default QuizScreen;
